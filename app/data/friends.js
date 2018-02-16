a67d74316e62ead1f0d1c51cc6ea7672505a8657
Katie Poeltler <katiepoeltler2018u.northwestern.edu>
2018-02-16 15:50:32 -0600

    added array of objects to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+let friends = [
+    {
+        'name': 'John',
+        'photo': '',
+        'scores': []
+    },
+    {
+        'name': 'Matthew',
+        'photo': '',
+        'scores': []
+    },
+    {
+        'name': 'Ginger',
+        'photo': '',
+        'scores': []
+    }
+
+];
+
+
+module.exports = friends;
