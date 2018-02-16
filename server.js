23cacdcd533a96c491aa04edebfda96bf0736ce3
Katie Poeltler <katiepoeltler2018u.northwestern.edu>
2018-02-15 19:23:30 -0600

    set up the server and files

---
+++ server.js
@@ -0,0 +1,28 @@
+//Dependencies
+var http = require('http');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var server = http.createServer(handleRequest)
+
+// Sets up the Express app
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//sets up the server port
+function handleRequest(request, response) {
+    // Send the below string to the client when the user visits the PORT URL
+    response.end('It Works!! Path Hit: ' + request.url)
+  }
+  
+  
+//enables functionality to the server
+server.listen(PORT, function() {
+   
+      console.log('Server listening on: http://localhost:' + PORT)
+    });
+    
