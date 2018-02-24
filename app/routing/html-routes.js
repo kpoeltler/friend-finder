36c28bd2d708e33b406f272ab571e7d1b26a19dc
Katie Poeltler <kpoeltler@gmail.com>
2018-02-24 01:28:47 -0600

    api

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -5,13 +5,13 @@ let bodyParser = require("body-parser");
 let path = require("path");
 
 module.exports = function(app) {
-  // express data parsing
+  
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.text());
   app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-  // routes
+  
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
