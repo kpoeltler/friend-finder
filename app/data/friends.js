cad4bb262b60f1acd9928f59f92a1e0b6008c17e
Katie <email@example.com>
2018-05-17 13:44:18 -0500

    fixing array

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,37 +4,42 @@ module.exports = [
   name: "Moshi",
   link:"https://www.instagram.com/moshi.moshi.yumi/?hl=en",
   photo: "images/moshi.jpg",
-  scores: [1, 3, 5, 1, 5, 1, 2, 4, 1, 1],
+  scores: [1, 3, 5, 1, 5, 1, 2, 4, 1, 1], 
+  // 29
 },
 {
   name: "Goldie",
   link:"https://www.instagram.com/puppydairy/?hl=en",
   photo: "images/goldie.jpg",
   scores: [5, 5, 5, 5, 2, 2, 5, 5, 2, 2],
+  // 38
 },
 {
   name: "Pippa",
   link:"https://www.instagram.com/pippathepup18/?hl=en",
   photo: "images/pippa.jpg",
   scores: [1, 2, 2, 2, 3, 5, 2, 2, 2, 2],
+  // 22
 },
 {
   name: "Boxer",
   link:"https://www.instagram.com/boxerparadise/?hl=en",
   photo: "images/boxer.jpg",
   scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  // 30
 },
 {
   name: "Yorkie",
   link:"https://www.instagram.com/hermi_da_yorkie/?hl=en",
   photo: "images/yorkie.jpg",
   scores: [5, 3, 2, 4, 2, 3, 2, 4, 1, 5],
+  // 31
 },
 {
   name: "Graham",
   link:"https://www.instagram.com/graham_the_oes/?hl=en",
   photo: "images/graham.jpg",
-  scores: [1, 1, 1, 5, 1, 5, 3, 3, 3, 5],
+  scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 },
 {
   name: "Sunny",
@@ -46,13 +51,15 @@ module.exports = [
 name: "Marnie, knows all the Hollywood elite, just check out all his selfies.",
   link: "https://www.instagram.com/marniethedog/?hl=en",
   photo: "images/marnie.jpeg",
-  scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+  scores: [4, 5, 5, 4, 4, 1, 4, 4, 1, 1],
+  // 34
 },
 {
   name: "Missasiaxoxo, rocks as hard as her owner Lady Gaga!",
  link:"https://www.instagram.com/p/BPLUNKxD5yu/?taken-by=missasiaxoxo",
  photo: "images/gaga.jpg",
- scores: [4, 4, 4, 1, 4, 1, 4, 4, 1, 4],
+ scores: [4, 1, 4, 1, 1, 1, 1, 4, 1, 1],
+//  19
 }
 
 ];
