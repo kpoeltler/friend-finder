50bbaa47ad911a42cf1b021113f81ecbea55d5c8
Katie <email@example.com>
2018-05-17 13:24:38 -0500

    celebrity

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,3 +8,20 @@ module.exports = function(app) {
     res.sendFile("survey.html", {root: "./app/public"});
   });
 }
+
+// =================================
+// var path = require("path");
+
+// var results = require("../data/friends.js");
+
+
+// module.exports = function(app) {
+
+//   app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/home.html"));
+//   });
+
+//   app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/survey.html"));
+//   });
+// };
