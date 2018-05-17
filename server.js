8616851779aa1e5586c36a915f4c4f69e5442f21
Katie <email@example.com>
2018-05-17 07:59:37 -0500

    server

--- server.js
+++ server.js
@@ -1,37 +1,18 @@
-// Dependencies
-// =============================================================
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
-let path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// Sets up the Express App
-// =============================================================
-
-let PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
-app.use(express.static("app/public"));
-
-// Routes WHY THIS METHOD
-// =============================================================
-// app.all('app/routing/api-routes.js', function (req, res, next) {
-//   console.log('accessing api-routes')
-//   next() // pass control to the next handler
-// })
-// app.all('app/routing/html-routes.js', function (req, res, next) {
-//   console.log('accessing html-routes')
-// next() // pass control to the next handler
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 require("./app/routing/api-routes.js")(app);
-
 require("./app/routing/html-routes.js")(app);
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
