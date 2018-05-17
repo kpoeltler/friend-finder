fffd5a710b005c5498b3438097cc6ca649baba7e
Katie <email@example.com>
2018-05-17 12:10:40 -0500

    changed image to photo

--- app/public/javascript/survey.js
+++ app/public/javascript/survey.js
@@ -1,35 +1,35 @@
-$("#submit-btn").on("click", function() {
-    event.preventDefault();
-    
+$("#submit-btn").on("click", function () 
+{
+  event.preventDefault();
+
     var getScores = [];
-  
-
     for (var i = 0; i < 10; i++) {
-      getScores.push($("#question-" + (i+1)).val() * 1);
-    }
-  
+    getScores.push($("#question-" + (i + 1)).val() * 1);
+}
 
-    var requestData = {
-      scores: JSON.stringify(getScores)
-    };
-  
 
-    $.post("/api/friends", requestData)
-      .then((data) => {
-        var link = $("<a>");
-        var image = $("<img>");
-        link.attr("href", data.link);
-        link.attr("target", "_blank");
-        link.text("Follow me on Instagram!")
-        image.attr("src", data.photo);
-        image.attr("alt", "picture of " + data.name);
-        image.attr("style", "width: 100%");
-  
+  var requestData = 
+  {
+    scores: JSON.stringify(getScores)
+  };
 
-        $("#result-name").empty();
-        $("#result-display").empty();
-        $("#result-name").text(data.name);
-        $("#result-display").append(link);
-        $("#result-display").append(image);
-      });
-  });
+
+  $.post("/api/friends", requestData)
+    .then((data) => {
+      var link = $("<a>");
+      var photo = $("<img>");
+      link.attr("href", data.link);
+      link.attr("target", "_blank");
+      link.text("Follow me on Instagram!")
+      photo.attr("src", data.photo);
+      photo.attr("alt", "picture of " + data.name);
+      photo.attr("style", "width: 100%");
+
+
+      $("#result-name").empty();
+      $("#result-display").empty();
+      $("#result-name").text(data.name);
+      $("#result-display").append(link);
+      $("#result-display").append(photo);
+    });
+});
