ea57b5a7e87c4ae06d3f94f8d8c25ad7827ba3c5
Katie <email@example.com>
2018-05-17 13:18:08 -0500

    celeb pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -41,6 +41,19 @@ module.exports = [
   link: "https://www.instagram.com/explore/tags/sunnydog/?hl=en",
   photo: "images/sunny.jpg",
   scores: [3, 2, 2, 2, 3, 5, 2, 3, 3, 3]
+},
+{
+name: "Marnie, knows all the Hollywood elite, just check out all his selfies.",
+  link: "https://www.instagram.com/marniethedog/?hl=en",
+  photo: "images/marnie.jpeg",
+  scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+},
+{
+  name: "Missasiaxoxo, rocks as hard as her owner Lady Gaga!",
+ link:"https://www.instagram.com/p/BPLUNKxD5yu/?taken-by=missasiaxoxo",
+ photo: "images/gaga.jpg",
+ scores: [4, 4, 4, 1, 4, 1, 4, 4, 1, 4]
 }
+
 ];
 
