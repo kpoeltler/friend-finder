6951a03aaebecef7ae338cd893009965f32ce5a4
Katie <email@example.com>
2018-05-17 07:45:21 -0500

    update api friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,40 +1,45 @@
-let friends = [
-    {
-      name: "Matthew Meyer",
-      photo: "https://teamcolorcodes.com/milwaukee-brewers-color-codes/",
-      scores: [3, 2, 2, 2, 3, 5, 2, 3, 3, 3]
-    },
-    {
-      name: "Bria",
-      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVOOWESltFvXAaHJ3g8QG6gyegui7fznEjTsGJpGQgpnJ7tfTy",
-      scores: [5, 3, 2, 4, 3, 3, 2, 5, 3, 4]
-    },
-    {
-      name: "Ginger",
-      photo: "https://pbs.twimg.com/profile_images/2340351663/3lm0s83caat3mj7h3d8n_400x400.jpeg",
-      scores: [5, 3, 4, 5, 2, 4, 5, 3, 2, 3]
-    },
-    {
-      name: "Dr. Jedidah Isler",
-      photo: "http://1.bp.blogspot.com/-x2QfgB5rhXU/VHY9HxDC98I/AAAAAAAATzU/jEMXj578Y84/s1600/Jedidah%2BIsler.jpeg",
-      scores: [1, 5, 2, 4, 3, 5, 3, 5, 1, 5]
-    },
-    {
-      name: "Marie",
-      photo: "https://www.biography.com/.image/t_share/MTE5NTU2MzE2MTkzNzE5ODE5/marie-curie-9263538-1-402.jpg",
-      scores: [3, 4, 2, 3, 1, 4, 3, 4, 1, 2]
-    },
-    {
-      name: "Laura",
-      photo: "http://1.bp.blogspot.com/-p4VhUNrPtV8/T8CecUs0xyI/AAAAAAAABG8/MY6088r0jys/s320/l.jpg",
-      scores: [5, 3, 2, 4, 2, 3, 2, 4, 1, 5]
-    },
-    {
-      name: "Lindsey",
-      photo: "http://www.wbur.org/artery/2017/03/13/silent-sky-harletd-astronomy-women-computers",
-      scores: [1, 2, 3, 5, 1, 5, 5, 4, 1, 3]
-    },
-  ];
-  
-  module.export = friends;
-  
+module.exports = [{
+    
+  name: "Sunny",
+  link: "https://www.instagram.com/explore/tags/sunnydog/?hl=en",
+  photo: "images/sunny.jpg",
+  scores: [3, 2, 2, 2, 3, 5, 2, 3, 3, 3]
+},
+{
+  name: "moshi",
+  link:"https://www.instagram.com/moshi.moshi.yumi/?hl=en",
+  photo: "images/moshi.jpg",
+  scores: [5, 3, 2, 4, 3, 3, 2, 5, 3, 4]
+},
+{
+  name: "Goldie",
+  link:"https://www.instagram.com/puppydairy/?hl=en",
+  photo: "images/goldie.jpg",
+  scores: [5, 3, 4, 5, 2, 4, 5, 3, 2, 3]
+},
+{
+  name: "Pippa",
+  link:"https://www.instagram.com/pippathepup18/?hl=en",
+  photo: "images/pippa.jpg",
+  scores: [1, 5, 2, 4, 3, 5, 3, 5, 1, 5]
+},
+{
+  name: "boxer",
+  link:"https://www.instagram.com/boxerparadise/?hl=en",
+  photo: "images/boxerjpg",
+  scores: [3, 4, 2, 3, 1, 4, 3, 4, 1, 2]
+},
+{
+  name: "yorkie",
+  link:"https://www.instagram.com/hermi_da_yorkie/?hl=en",
+  photo: "images/yorkie.jpg",
+  scores: [5, 3, 2, 4, 2, 3, 2, 4, 1, 5]
+},
+{
+  name: "graham",
+  link:"https://www.instagram.com/graham_the_oes/?hl=en",
+  photo: "images/graham.jpg",
+  scores: [1, 2, 3, 5, 1, 5, 5, 4, 1, 3]
+},
+];
+
