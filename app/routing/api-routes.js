9e1c782513398e71a92e4c157344e7227753279b
Katie <email@example.com>
2018-05-17 07:32:19 -0500

    update api routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,53 +1,31 @@
-
-
-
-    
+function totalDiff(array1, array2) {
+    var totalDifference = 0;
+    for(var j = 0; j < array1.length; j++) {
+      totalDifference += Math.abs(array1[j] - array2[j]);
+    }
+    return totalDifference;
+  }
+  
+  module.exports = function(app) {
+    var data = require("../data/friends.js");
+  
+    app.get("/api/friends", function (req, res) {
+      res.json(data);
+    });
+  
+    // Search for Specific Character (or all characters) - provides JSON
+    app.post("/api/friends", function (req, res) {
+      var getData = JSON.parse(req.body.scores);
+      var userMatch = 0;
+      var userLowest = 20;
       
-      var friendsData = require('../data/friends.js');
-      var bodyParser = require('body-parser');
-      var path = require('path');
-      
-      
-      module.exports = function(app) {
-      
-          
-          app.use(bodyParser.json());
-          app.use(bodyParser.urlencoded({ extended: true }));
-          app.use(bodyParser.text());
-          app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-      
-          
-          app.get("/data/friends", function(req, res) {
-              res.json();
-          });
-      
-       
-          app.post("/data/friends", function(req, res) {
-              
-              var userFriend = req.body;
-              var totalScore;
-              var compDiff = 0;
-              var compFriend;
-              
-              
-          
-              for (var i = 0; i < friendsData.length; i++) {
-                  
-                  totalScore = 0;
-                  
-                  for (var j = 0; j < 10; j++) {
-                      var minuend = userFriend[j];
-                      var subtrahend = friendsData[i].scores[j];
-                      parseInt(minuend);
-                      parseInt(subtrahend);
-                      
-                      var addToScore = Math.abs(minuend - subtrahend);
-                     
-                      totalScore = totalScore + addToScore;
-                  }
-                 
-                  var newTotalScore = totalScore;
-                }   
-          });
-      };
-      
+      for(var i = 0; i < data.length; i++) {
+        var difference = totalDiff(data[i].scores, getData);
+        if (difference < currentLowest) {
+          userLowest = difference;
+          userMatch = i;
+        }
+      }
+      res.json(data[userMatch]);
+    });
+  }
