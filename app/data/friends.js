e6ed17f8bbacf6985d0682e5ddf944a3cbbee27e
Katie <email@example.com>
2018-05-17 13:27:19 -0500

    celebri

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,49 +4,49 @@ module.exports = [
   name: "Moshi",
   link:"https://www.instagram.com/moshi.moshi.yumi/?hl=en",
   photo: "images/moshi.jpg",
-  scores: [1, 1, 1, 1, 1, 1, 2, 1, 1, 1]
+  scores: [1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
 },
 {
   name: "Goldie",
   link:"https://www.instagram.com/puppydairy/?hl=en",
   photo: "images/goldie.jpg",
-  scores: [5, 5, 5, 5, 2, 2, 5, 5, 2, 2]
+  scores: [5, 5, 5, 5, 2, 2, 5, 5, 2, 2],
 },
 {
   name: "Pippa",
   link:"https://www.instagram.com/pippathepup18/?hl=en",
   photo: "images/pippa.jpg",
-  scores: [1, 2, 2, 2, 3, 5, 2, 2, 2, 2]
+  scores: [1, 2, 2, 2, 3, 5, 2, 2, 2, 2],
 },
 {
   name: "Boxer",
   link:"https://www.instagram.com/boxerparadise/?hl=en",
   photo: "images/boxer.jpg",
-  scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 },
 {
   name: "Yorkie",
   link:"https://www.instagram.com/hermi_da_yorkie/?hl=en",
   photo: "images/yorkie.jpg",
-  scores: [5, 3, 2, 4, 2, 3, 2, 4, 1, 5]
+  scores: [5, 3, 2, 4, 2, 3, 2, 4, 1, 5],
 },
 {
   name: "Graham",
   link:"https://www.instagram.com/graham_the_oes/?hl=en",
   photo: "images/graham.jpg",
-  scores: [1, 1, 1, 5, 1, 5, 3, 3, 3, 5]
+  scores: [1, 1, 1, 5, 1, 5, 3, 3, 3, 5],
 },
 {
   name: "Sunny",
   link: "https://www.instagram.com/explore/tags/sunnydog/?hl=en",
   photo: "images/sunny.jpg",
-  scores: [3, 2, 2, 2, 3, 5, 2, 3, 3, 3]
+  scores: [3, 2, 2, 2, 3, 5, 2, 3, 3, 3],
 },
 {
 name: "Marnie, knows all the Hollywood elite, just check out all his selfies.",
   link: "https://www.instagram.com/marniethedog/?hl=en",
   photo: "images/marnie.jpeg",
-  scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 },
 {
   name: "Missasiaxoxo, rocks as hard as her owner Lady Gaga!",
